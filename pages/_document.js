fb5b7913db440145d2acedd665e032972e935f99
mallikauptechmind <mallikachauhan2118@gmail.com>
2026-07-23 16:59:46 +0530

    update

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,13 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;900&family=Outfit:wght@300;400;500;600;700;900&display=swap"
           rel="stylesheet"
         />
+
+
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        
         <meta name="theme-color" content="#050510" />
       </Head>
       <body>
